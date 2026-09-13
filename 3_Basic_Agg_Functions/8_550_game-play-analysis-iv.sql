461c7699c96bd64a70ff2e7d82ead3d4967fa1da
Bikash Adhikari <115802842+bikashadhikari1508@users.noreply.github.com>
2026-09-13 11:22:26 +0530

    Refactor SQL for player login fraction calculation
    solved by MS SQL Server

--- 3_Basic_Agg_Functions/8_550_game-play-analysis-iv.sql
+++ 3_Basic_Agg_Functions/8_550_game-play-analysis-iv.sql
@@ -5,3 +5,25 @@ WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) IN (
     SELECT player_id, MIN(event_date)
     FROM Activity
     GROUP BY player_id
+
+
+-- SOLVED BY MS SQL SERVER
+    
+WITH first_login AS
+(
+    SELECT
+        player_id,
+        MIN(event_date) AS first_date
+    FROM Activity
+    GROUP BY player_id
+)
+
+SELECT
+    ROUND(
+        1.0 * COUNT(a.player_id) / COUNT(f.player_id),
+        2
+    ) AS fraction
+FROM first_login f
+LEFT JOIN Activity a
+    ON f.player_id = a.player_id
+    AND DATEDIFF(day, f.first_date, a.event_date) = 1;
